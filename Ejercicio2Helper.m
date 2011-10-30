1d3a9a59ebe0fe18e7a65f4b4deb22501982daad
Diego Martín Correa <diegomartincorrea@gmail.com>
2011-10-30 16:39:29 -0300

    Preparando los datos

--- Ejercicio2Helper.m
+++ Ejercicio2Helper.m
@@ -1,7 +1,7 @@
 classdef Ejercicio2Helper
     
-    %EJERCICIO2HELPER Esta clase se encarga de armar diferentes necesidades
-    %del ejercicio n?mero del trabajo pr?ctico
+    %% EJERCICIO2HELPER Esta clase se encarga de armar diferentes necesidades
+    % del ejercicio n?mero del trabajo pr?ctico
     %
     %   Una de sus tareas es la generaci?n de una matriz a partir del
     %   vector clase del conjunto de datos vinos, seg?n correponda con la
@@ -15,12 +15,27 @@ classdef Ejercicio2Helper
     end
     
     methods(Static)
-        function [T] = TransformarClaseATansig(ConjuntoDatos)
+        %% Lee un archivo y retorna un dataset
+        function [D] = LeerArchivo(filename)
+            D = csvread(filename);
+        end
+        %% Lee un archivo y retorna desde 1 hasta CantFilas del dataset
+        function [D] = LeerFilasArchivo(filename, CantFilas)
+            D = csvread(filename);
+            D = D(1:CantFilas,:);
+        end
+        %% Prepara Un conjunto de datos del dataset drug5.csv para que la clase
+        % sea Tansig
+        function [nuevaClase] = TransformarClase(CD, Funcion)
             
-            T = ConjuntoDatos.Clase;
+            modificador = min(feval(Funcion,'output'));
+            
+            nuevaClase = ones(CD.CantidadPatrones, 5) * modificador;
+            for index = 1:CD.CantidadPatrones
+                nuevaClase(index, CD.Clase(index)) = 1;
+            end
+            
         end
     end
-    
-    
 end
 
--- ejercicio2a.m
+++ ejercicio2a.m
@@ -5,7 +5,14 @@ clc
 
 %% Lectura de los datos de entrada: Archivos drugs.csv
 
-CD = ConjuntoDatos(csvread('drug5.csv'), 1, 6, 7);
+CD = ConjuntoDatos(Ejercicio2Helper.LeerFilasArchivo('drug5.csv', 10), 1, 6, 7);
 CD.Escalar;
 
-CD.Datos
+% CD.Datos
+% CD.Patrones
+%%CD.Clase
+
+Ejercicio2Helper.TransformarClase(CD, 'tansig')
+Ejercicio2Helper.TransformarClase(CD, 'logsig')
+
+
